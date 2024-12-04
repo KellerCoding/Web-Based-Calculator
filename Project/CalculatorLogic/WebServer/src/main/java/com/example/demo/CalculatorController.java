1d10779948ac457266230eb9124ad9d1b8423abc
Glurp <ktkellerbusiness@gmail.com>
2024-12-03 22:18:13 -0500

    Hotfix - Pre End 2 End Configuration
    Simply to stamp all changes made pre end to end to preserve code condition

--- Project/CalculatorLogic/WebServer/src/main/java/com/example/demo/CalculatorController.java
+++ Project/CalculatorLogic/WebServer/src/main/java/com/example/demo/CalculatorController.java
@@ -4,6 +4,7 @@ import Calc.*;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,14 +15,20 @@ import java.util.List;
 @Controller
 public class CalculatorController {
 
-    /*public void configureViewResolvers(ViewResolverRegistry registry) {
+    //Honestly not sure what this does
+    public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp("/WEB-INF/pages/", ".jsp");
-    }*/
+    }
+
+    //Method to return
     public static void returnColor(CalculationResult result, Model model){
         if (!result.getIsSuccess()){
             model.addAttribute("color", "#B70F0A");
-            model.addAttribute("textcolor","#FFFFFF");
-        }else model.addAttribute("color", "#FFECD7");
+            model.addAttribute("textcolor",false);
+        }else{
+            model.addAttribute("textcolor",true);
+            model.addAttribute("color", "#FFECD7");
+        }
     }
 
     @GetMapping("/result")
@@ -52,6 +59,7 @@ public class CalculatorController {
         switch (input) {
 
 
+            //Here is the button value returned for the Sample Standard Deviation button function
                 case "compSTD":
                     for (int i = 0; i < length; i++) {
                         list.add(Double.parseDouble(stringList.get(i)));
@@ -69,6 +77,7 @@ public class CalculatorController {
                     break;
 
 
+            //Button value returned for the Population Standard Deviation button function
                 case "compPop":
                     for (int i = 0; i < length; i++) {
                         list.add(Double.parseDouble(stringList.get(i)));
@@ -85,6 +94,7 @@ public class CalculatorController {
                     break;
 
 
+            //Button value returned for the Mean button function
                 case "compMean":
                     for (int i = 0; i < length; i++) {
                         list.add(Double.parseDouble(stringList.get(i)));
@@ -102,6 +112,7 @@ public class CalculatorController {
                     break;
 
 
+            //Button value returned for the Z-Score button function
                 case "compZ":
                     result = CalculatorMethods.computeZScore(stringList.getFirst());
                     if (!result.getIsSuccess()) {
@@ -116,6 +127,7 @@ public class CalculatorController {
                     break;
 
 
+            //Button value returned for the Single Linear Regression Formula button function
                 case "compSingle":
                     String[] stringArray = new String[length];
                     for (int i = 0; i < length; i++) {
@@ -135,6 +147,7 @@ public class CalculatorController {
                     break;
 
 
+            //Button value returned for the Single Linear Regression Prediction button function
                 case "compY":
                     result = CalculatorMethods.predictY(stringList.getFirst());
                     if (!result.getIsSuccess()) {
@@ -149,8 +162,9 @@ public class CalculatorController {
                     break;
 
             }
+            //The cases for invalid input are all covered here in this Catch
             } catch (Exception e){
-            model.addAttribute("something", "Invalid Input - Clear and Try Again\n"+result.getError());
+            model.addAttribute("something", "Invalid Input - Clear and Try Again\n");
             model.addAttribute("color", "#B70F0A");
             model.addAttribute("textcolor","#FFFFFF");
             }
@@ -159,6 +173,7 @@ public class CalculatorController {
         return "result";
 }
 
+//The clear button returns the page to the initial page
     @RequestMapping("/index")
     public String index(Model model, @RequestParam() Boolean button){
 
